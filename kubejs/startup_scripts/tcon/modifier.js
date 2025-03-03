0c5c67ffbc439bfa5b5e90c6c7845669ae945d1b
Neha YoruNina <836193631@qq.com>
2025-03-03 17:38:52 +0800

    按键激活过滤部分请求

---
+++ kubejs/startup_scripts/tcon/modifier.js
@@ -0,0 +1,8 @@
+// priority: 500
+// TConJSEvents.modifierRegistry(event => {
+//     event.createNew('test', builder => {
+//         builder.onAfterMeleeHit((view, damage, context) => {
+//             context.attacker.getOffHandItem().get
+//         })
+//     })
+// })
