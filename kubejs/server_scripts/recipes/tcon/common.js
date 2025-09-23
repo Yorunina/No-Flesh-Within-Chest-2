7c2f8ee1483f69e18d66c9cfe07214c3c7d71aea
HideUsSaveUs <2781612791@qq.com>
2025-09-23 21:45:04 +0800

    为部分血肉重铸物品做了简单的匠魂兼容并未细调数值

--- kubejs/server_scripts/recipes/tcon/common.js
+++ kubejs/server_scripts/recipes/tcon/common.js
@@ -1,13 +1,26 @@
 // priority: 500
 ServerEvents.recipes(event => {
     const { tconstruct } = event.recipes
-    //鲜活血肉
+    //无流体物品
     tconstruct.material('kubejs:living_flesh', 'biomancy:living_flesh')
     tconstruct.material('kubejs:creator_mix', 'biomancy:creator_mix')  
+    tconstruct.material('kubejs:koboleton_bone', 'cataclysm:koboleton_bone') 
+    tconstruct.material('kubejs:mob_marrow', 'biomancy:mob_marrow') 
+    tconstruct.material('kubejs:bone_fragments', 'biomancy:bone_fragments')
+    tconstruct.material('kubejs:tough_fibers', 'biomancy:tough_fibers') 
+    tconstruct.material('kubejs:elastic_fibers', 'biomancy:elastic_fibers')  
+    tconstruct.material('kubejs:withered_mob_marrow', 'biomancy:withered_mob_marrow') 
+    tconstruct.material('kubejs:magebloom_fiber', 'ars_nouveau:magebloom_fiber') 
+    tconstruct.material('kubejs:mob_sinew', 'biomancy:mob_sinew') 
+    tconstruct.material('kubejs:nutrients', 'biomancy:nutrients') 
+    tconstruct.material('kubejs:organic_matter', 'biomancy:organic_matter') 
+    tconstruct.material('kubejs:regenerative_fluid', 'biomancy:regenerative_fluid') 
+    tconstruct.material('', '') 
+    tconstruct.material('', '') 
+    tconstruct.material('', '') 
     //暗铁
     tconstruct.material('kubejs:dark_iron', 'graveyard:dark_iron_ingot', 1, 1)
     tconstruct.material('kubejs:dark_iron', 'graveyard:dark_iron_block', 9, 1, 'graveyard:dark_iron_ingot')
-
     tconstruct.material_fluid('kubejs:dark_iron', Fluid.of('kubejs:melted_dark_iron', 90), 800)
     tconstruct.melting(Fluid.of('kubejs:melted_dark_iron', 90), Ingredient.of('graveyard:dark_iron_ingot'), 800, 100)
     tconstruct.melting(Fluid.of('kubejs:melted_dark_iron', 810), Ingredient.of('graveyard:dark_iron_block'), 800, 900)
@@ -17,7 +30,6 @@ ServerEvents.recipes(event => {
     //凋零合金
     tconstruct.material('kubejs:witherite_ingot', 'cataclysm:witherite_ingot', 1, 1)
     tconstruct.material('kubejs:witherite_ingot', 'cataclysm:witherite_block', 9, 1, 'cataclysm:witherite_ingot')
-
     tconstruct.material_fluid('kubejs:witherite_ingot', Fluid.of('kubejs:melted_witherite', 90), 800)
     tconstruct.melting(Fluid.of('kubejs:melted_witherite', 90), Ingredient.of('cataclysm:witherite_ingot'), 800, 100)
     tconstruct.melting(Fluid.of('kubejs:melted_witherite', 810), Ingredient.of('cataclysm:witherite_block'), 800, 900)
@@ -27,7 +39,6 @@ ServerEvents.recipes(event => {
     //远古金属
     tconstruct.material('kubejs:ancient_metal', 'cataclysm:ancient_metal_ingot', 1, 1)
     tconstruct.material('kubejs:ancient_metal', 'cataclysm:ancient_metal_block', 9, 1, 'cataclysm:ancient_metal_ingot')
-
     tconstruct.material_fluid('kubejs:ancient_metal', Fluid.of('kubejs:melted_ancient_metal', 90), 800)
     tconstruct.melting(Fluid.of('kubejs:melted_ancient_metal', 90), Ingredient.of('cataclysm:ancient_metal_ingot'), 800, 100)
     tconstruct.melting(Fluid.of('kubejs:melted_ancient_metal', 810), Ingredient.of('cataclysm:ancient_metal_block'), 800, 900)
@@ -39,7 +50,6 @@ ServerEvents.recipes(event => {
     //黑钢
     tconstruct.material('kubejs:black_steel', 'cataclysm:black_steel_ingot', 1, 1)
     tconstruct.material('kubejs:black_steel', 'cataclysm:black_steel_block', 9, 1, 'cataclysm:black_steel_ingot')
-
     tconstruct.material_fluid('kubejs:black_steel', Fluid.of('kubejs:melted_black_steel', 90), 800)
     tconstruct.melting(Fluid.of('kubejs:melted_black_steel', 90), Ingredient.of('cataclysm:black_steel_ingot'), 800, 100)
     tconstruct.melting(Fluid.of('kubejs:melted_black_steel', 810), Ingredient.of('cataclysm:black_steel_block'), 800, 900)
@@ -48,10 +58,8 @@ ServerEvents.recipes(event => {
     tconstruct.casting_table('cataclysm:black_steel_nugget', Fluid.of('kubejs:melted_black_steel', 10), 47, Ingredient.of('#tconstruct:casts/multi_use/nugget'), false)
     tconstruct.casting_table('cataclysm:black_steel_nugget', Fluid.of('kubejs:melted_black_steel', 10), 47, Ingredient.of('#tconstruct:casts/single_use/nugget'), true)
     tconstruct.casting_basin('cataclysm:black_steel_block', Fluid.of('kubejs:melted_black_steel', 810), 180)
-    //腾炎锭
     tconstruct.material('kubejs:ignitium', 'cataclysm:ignitium_ingot', 1, 1)
     tconstruct.material('kubejs:ignitium', 'cataclysm:ignitium_block', 9, 1, 'cataclysm:ignitium_ingot')
-
     tconstruct.material_fluid('kubejs:ignitium', Fluid.of('kubejs:melted_ignitium', 90), 800)
     tconstruct.melting(Fluid.of('kubejs:melted_ignitium', 90), Ingredient.of('cataclysm:ignitium_ingot'), 800, 100)
     tconstruct.melting(Fluid.of('kubejs:melted_ignitium', 810), Ingredient.of('cataclysm:ignitium_block'), 800, 900)
