b44eeb8db72797fd7534a13ad8a28d5461f126b0
Neha YoruNina <836193631@qq.com>
2025-12-06 08:05:42 +0800

    重返黄金乡 #1

--- kubejs/server_scripts/domain/dimension/item_in_nether_portal.js
+++ kubejs/server_scripts/domain/dimension/item_in_nether_portal.js
@@ -1,24 +1,31 @@
 // priority: 500
-InfinityEvents.itemInNetherPortal(event => {
+InfinityEvents.itemInPortal(event => {
     const itemEntity = event.getEntity()
     const itemStack = itemEntity.getItem()
     const level = event.getLevel()
     const pos = event.getPos()
-
+    console.log(itemStack)
     if (itemStack.is('kubejs:key_to_infinity')) {
-        let itemName = itemStack.getHoverName()
-        let nameString = itemName.getString()
+        let nameString = 'infinity:random'
+        if (itemStack.hasCustomHoverName()) {
+            nameString = itemStack.getHoverName().getString()
+        }
         if (!nameString.startsWith('infinity:')) {
             nameString = 'infinity:'.concat(nameString.trim())
         }
-        let isSucc = InfinityPortalCreator.modifyOnInitialCollision(nameString, level, pos)
-        if (isSucc) itemEntity.remove('changed_dimension')
+        itemEntity.remove('changed_dimension')
+        InfinityPortalCreator.tryCreatePortalById(nameString, level, pos)
     } else if (itemStack.hasTag('lightmanscurrency:coins')) {
-        InfinityPortalCreator.modifyOnInitialCollision('kubejs:oath', level, pos)
+        InfinityPortalCreator.tryCreatePortalById('kubejs:oath', level, pos)
     }
 })
 
 
 InfinityEvents.infinityDimAdded(event => {
-
+    const id = event.getId()
+    const dim = event.getTargetDim()
+    console.log(id.toString())
+    if (id.toString() == 'infinity:test1') {
+        dim.structure_ids.put('ctov:medium/village_plains', ['infinity:medium_village_plains_-1149656568'])
+    }
 })
