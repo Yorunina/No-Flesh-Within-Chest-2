a431fab37ec3f51f5725816ce43b224ade6b0d65
Neha YoruNina <836193631@qq.com>
2026-02-14 03:20:27 +0800

    引入BioFactory，启动培养仓章节

--- kubejs/client_scripts/domain/jei/remove.js
+++ kubejs/client_scripts/domain/jei/remove.js
@@ -1,6 +1,6 @@
 // priority: 500
 JEIEvents.removeCategories(event => {
-    event.remove(['kubejs:eternal_altar', 'kubejs:mantle_energy_extractor'])
+    event.remove(['kubejs:eternal_altar', 'kubejs:mantle_energy_extractor', 'kubejs:growth_vat'])
 })
 
 // JEIEvents.removeRecipes(event => {
---
+++ kubejs/server_scripts/recipes/machine/growth_vat/growth_vat.js
@@ -0,0 +1,4 @@
+// priority: 500
+ServerEvents.recipes(event => {
+    // event.recipes.custommachinery.custom_machine('kubejs:growth_vat', 3600)
+})
