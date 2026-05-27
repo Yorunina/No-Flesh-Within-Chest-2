8239cc0637416e52abea4caaeb3442c80424bf6a
Neha YoruNina <836193631@qq.com>
2026-05-27 13:43:51 +0800

    Qol内容

--- kubejs/server_scripts/domain/common/colorful_slime.js
+++ kubejs/server_scripts/domain/common/colorful_slime.js
@@ -1,6 +1,6 @@
 // priority: 500
 LootJS.modifiers(event => {
-    event.addLootTypeModifier(LootType.ENTITY)
-        .entityPredicate(entity => entity.type == 'minecraft:slime' && entity instanceof $LivingEntity && entity.getActiveEffects().size() >= 7)
+    event.addEntityLootModifier('minecraft:slime')
+        .entityPredicate(entity => entity instanceof $LivingEntity && entity.getActiveEffects().size() >= 7)
         .addLoot(LootEntry.of('kubejs:colorful_slime_ball'))
 })
